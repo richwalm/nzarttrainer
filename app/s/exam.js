1c6083965f4180087a8ab8df43161d9961ffc840
Richard Walmsley <richwalm@gmail.com>
2024-03-31 17:57:27 +1300

    Converted to a Docker container.
    Still could be used without it though.
    Also improved the JavaScript.

--- app/s/exam.js
+++ app/s/exam.js
@@ -1,22 +1,21 @@
 // @license magnet:?xt=urn:btih:b8999bbaf509c08d127678643c515b9ab0836bae&dn=ISC.txt ISC-License
-var Answered = 0, AnsweredCorrectly = 0;
-var Total;
-var AnsweredArray;
+'use strict';
+let Answered = 0, AnsweredCorrectly = 0;
+let Total;
+let AnsweredArray;
 
-var URL = new URL(window.location.href);
+let RealTime;
+let TimeLimit;
 
-var RealTime = URL.searchParams.get('rt');
-var TimeLimit = URL.searchParams.get('tl');
+let AnsweredStatus;
+let CountDown;
 
-var AnsweredStatus;
-var CountDown;
-
-var End;
+let End;
 
 function Click() {
-	var Node = this;
+	let Node = this;
 
-	var Question = this.name;
+	let Question = this.name;
 
 	if (AnsweredArray[Question])
 		return;
@@ -31,15 +30,15 @@ function Click() {
 		Node = Node.parentNode;
 	} while (Node.nodeName != 'UL')
 
-	var Inputs = Node.getElementsByTagName('input');
-	for (var i = 0; i < Inputs.length; ++i) {
+	let Inputs = Node.getElementsByTagName('input');
+	for (let i = 0; i < Inputs.length; ++i) {
 		if (RealTime)
 			Inputs[i].disabled = true;
 	}
 
-	var Answer = this.value;
+	let Answer = this.value;
 
-	var Correct = (Answers[Question] == Answer ? true : false);
+	let Correct = (Answers[Question] == Answer ? true : false);
 
 	do {
 		Node = Node.parentNode;
@@ -57,7 +56,7 @@ function Click() {
 		AnsweredCorrectly++;
 	} else {
 		Node.style.textDecoration = 'line-through';
-		for (i = 0; i < Inputs.length; ++i)
+		for (let i = 0; i < Inputs.length; ++i)
 			if (Inputs[i].value == Answers[Question]) {
 				Node = Inputs[i];
 				do {
@@ -74,15 +73,15 @@ function Click() {
 		clearInterval();
 		window.onbeforeunload = null;
 		BeforePOST();
-		var Form = document.getElementsByTagName('form')[0];
+		const Form = document.getElementsByTagName('form')[0];
 		Form.submit();
 	}
 }
 
 function BeforePOST() {
-	var Checkboxes = document.getElementsByTagName('input');
-	for (var i = 0; i < Checkboxes.length; ++i) {
-		var Checkbox = Checkboxes[i];
+	const Checkboxes = document.getElementsByTagName('input');
+	for (let i = 0; i < Checkboxes.length; ++i) {
+		let Checkbox = Checkboxes[i];
 		if (Checkbox.type == 'radio') {
 			Checkbox.disabled = false;
 			Checkbox.onclick = function() { return false };
@@ -91,24 +90,24 @@ function BeforePOST() {
 }
 
 function Tick() {
-	var Diff = End - Date.now();
+	let Diff = End - Date.now();
 	if (Diff < 1000) {
 		CountDown.textContent = 'Time\'s up!';
 		clearInterval();
 		window.onbeforeunload = null;
 		BeforePOST();
-		var Form = document.getElementsByTagName('form')[0];
+		const Form = document.getElementsByTagName('form')[0];
 		Form.submit();
 		return;
 	}
 
 	Diff /= 1000;
 
-	var Hours = Diff / 60 / 60 | 0;
-	var Minutes = (Diff / 60 | 0) % 60;
+	let Hours = Diff / 60 / 60 | 0;
+	let Minutes = (Diff / 60 | 0) % 60;
 	if (Minutes < 10)
 		Minutes = '0' + Minutes;
-	var Seconds = (Diff | 0) % 60;
+	let Seconds = (Diff | 0) % 60;
 	if (Seconds < 10)
 		Seconds = '0' + Seconds;
 
@@ -116,9 +115,13 @@ function Tick() {
 }
 
 function OnLoad() {
-	var Inputs = document.getElementsByTagName('input');
-	for (var i = 0; i < Inputs.length; ++i) {
-		var Input = Inputs[i];
+	let URLO = new URL(window.location.href);
+	RealTime = URLO.searchParams.get('rt');
+	TimeLimit = URLO.searchParams.get('tl');
+
+	const Inputs = document.getElementsByTagName('input');
+	for (let i = 0; i < Inputs.length; ++i) {
+		let Input = Inputs[i];
 		if (Input.type == 'radio')
 			Input.onclick = Click;
 		else if (Input.type == 'submit') {
@@ -126,16 +129,16 @@ function OnLoad() {
 		}
 	}
 	if (RealTime) {
-		var Form = document.getElementsByTagName('form')[0];;
+		const Form = document.getElementsByTagName('form')[0];
 		Form.addEventListener('submit', BeforePOST);
 	}
 
 	Total = document.getElementsByTagName('fieldset').length;
 	AnsweredArray = [];
 
-	var Status = document.createElement('div');
+	const Status = document.createElement('div');
 	Status.id = 'status';
-	var StatusHTML = '<span>' + (RealTime ? AnsweredCorrectly + '/' : '') + Answered + '/' + Total + '</span>'
+	let StatusHTML = '<span>' + (RealTime ? AnsweredCorrectly + '/' : '') + Answered + '/' + Total + '</span>'
 
 	if (TimeLimit)
 		StatusHTML += ' &mdash; <span><span>'
@@ -155,7 +158,7 @@ function OnLoad() {
 }
 
 // Here be spoilers!
-var Answers =
+const Answers =
 [2,2,1,2,0,0,3,0,0,3,1,2,0,0,1,0,3,3,2,1,3,2,2,2,1,1,3,2,3,0,3,0,2,3,1,2,0,1,3,0,2,1,3,0,1,3,1,1,1,2,1,0,3,3,0,0,2,1,2,3,2,1,2,1,0,2,3,1,2,1,3,1,1,2,3,2,3,1,0,3,2,0,0,1,0,0,1,3,2,2,3,2,2,0,1,1,0,0,2,1,0,3,2,0,1,3,1,3,2,3,3,3,2,3,0,0,1,2,1,2,2,0,1,0,1,3,3,0,1,1,0,1,3,3,0,3,3,3,3,0,0,3,1,2,2,0,3,0,2,0,0,1,3,2,0,3,3,1,0,2,2,0,3,2,0,1,1,2,2,0,2,0,1,2,1,3,0,0,3,1,1,0,1,0,3,2,1,3,2,0,0,1,3,1,3,2,2,3,3,1,0,0,3,0,0,0,3,1,0,1,1,2,0,0,1,1,0,2,1,1,3,1,0,1,0,1,0,1,2,1,1,3,0,2,3,0,2,3,1,2,2,3,1,1,2,1,1,1,0,3,2,1,0,3,2,3,0,3,2,1,0,1,2,2,1,3,0,2,0,2,3,2,2,3,1,2,0,2,1,1,1,3,0,2,0,3,0,1,0,2,2,0,2,1,3,3,1,2,1,2,0,2,0,1,1,0,3,1,1,2,0,2,1,2,2,3,2,3,3,3,0,0,3,2,1,1,2,2,1,3,2,0,0,3,1,2,0,2,2,1,2,1,2,2,1,3,0,2,1,0,0,3,1,2,3,0,1,2,2,1,0,2,2,3,0,0,3,1,0,2,1,2,3,3,1,2,3,1,2,0,3,1,1,0,0,1,3,2,1,2,1,3,0,0,3,3,1,1,2,3,1,3,2,0,2,3,0,2,2,3,0,1,0,1,1,3,0,1,0,1,1,2,2,0,0,3,2,0,3,1,2,0,1,2,3,0,1,2,3,0,2,1,3,3,0,1,0,1,0,1,2,2,3,1,1,2,2,3,2,0,0,0,2,3,0,0,2,0,3,3,0,1,2,2,3,0,2,3,2,1,1,2,1,3,0,1,3,3,3,0,0,1,0,0,1,0,2,1,1,1,2,1,1,2,1,2,3,3,1,0,1,2,1,3,3,0,2,2,1,2,0,0,2,2,3,0,3,1,3,3,3,3,3,0,2,1,0,3,1,1,1,1,0,2,2,2,3,3,3,1,0,3,1,0,2,2,1,2,0,1,1,2,0,3,1,2,3,1,3,0,0,1,3,1,2,1,2,0,0,3,1,2,2,1,3,1,0,0,2,3,0,2,3,3,2,0,1,1,0,2];
 
 window.onload = OnLoad;
